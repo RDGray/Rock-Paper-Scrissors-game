d8efb4023cfa5e7574b8833248ba5d3039086f8e
RDGray <70549101+RDGray@users.noreply.github.com>
2021-09-20 02:49:38 +0300

    Basic functionality
    Basic functionality finalized

--- app/js/script.js
+++ app/js/script.js
@@ -1,35 +1,128 @@
-const userScore = 0;
-const computerScore = 0;
-const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("computer-score");
+let userScore = 0;
+let computerScore = 0;
+let userScore_span = document.getElementById("user-score");
+let computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".game__score-board");
-const result_div = document.querySelector(".game__result");
+let result_p = document.querySelector(".game__text--result");
+let resultUser_p = document.querySelector(".game__text--user");
+let resultComputer_p = document.querySelector(".game__text--computer");
+let hpUser = document.querySelector(".game__hp_bar_user");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scrissors_div = document.getElementById("s");
 
+let nani_user = document.getElementById("nani_user");
+let nani_computer = document.getElementById("nani_computer");
+
+//Player choice on click
+function main() {
+  rock_div.addEventListener("click", () => {
+    resultUser_p.style.display = "block";
+    resultComputer_p.style.display = "block";
+
+    game("r");
+  });
+
+  paper_div.addEventListener("click", () => {
+    resultUser_p.style.display = "block";
+    resultComputer_p.style.display = "block";
+    game("p");
+  });
+
+  scrissors_div.addEventListener("click", () => {
+    resultUser_p.style.display = "block";
+    resultComputer_p.style.display = "block";
+    game("s");
+  });
+}
+
+main();
+
+//random choice for computer
 function getComputerChoice() {
   const choices = ["r", "p", "s"];
   const randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber];
 }
 
+//Player and comp choices
 function game(userChoice) {
-  console.log("zz " + userChoice);
+  const computerChoice = getComputerChoice();
+
+  switch (userChoice + computerChoice) {
+    //Win scenarios
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, computerChoice);
+      break;
+    //lose scenarios
+    case "sr":
+    case "rp":
+    case "ps":
+      lose(userChoice, computerChoice);
+      break;
+    //Draw scenarios
+    case "rr":
+    case "pp":
+    case "ss":
+      draw(userChoice, computerChoice);
+      break;
+  }
 }
 
-function main() {
-  rock_div.addEventListener("click", () => {
-    game("r");
-  });
+function convertToWord(letter) {
+  if (letter === "r") return "Rock";
 
-  paper_div.addEventListener("click", () => {
-    game("p");
-  });
+  if (letter === "p") return "Paper";
 
-  scrissors_div.addEventListener("click", () => {
-    game("s");
-  });
+  if (letter === "s") return "Scrissors";
 }
 
-main();
+//Win, Lose, Draw behaviour
+function win(userChoice, computerChoice) {
+  const userChoice_div = document.getElementById(userChoice);
+
+  userScore++;
+  userScore_span.innerHTML = userScore;
+
+  computerScore_span.innerHTML = computerScore;
+
+  resultUser_p.innerHTML = `You pick  ${convertToWord(userChoice)}`;
+  resultComputer_p.innerHTML = `Enemy picks ${convertToWord(computerChoice)}`;
+  result_p.innerHTML = `You Win!`;
+
+  userChoice_div.classList.add("green-glow");
+  setTimeout(() => {
+    userChoice_div.classList.remove("green-glow");
+  }, 400);
+}
+
+function lose(userChoice, computerChoice) {
+  const userChoice_div = document.getElementById(userChoice);
+  computerScore++;
+  computerScore_span.innerHTML = computerScore;
+
+  userScore_span.innerHTML = userScore;
+
+  resultUser_p.innerHTML = `You pick ${convertToWord(userChoice)}`;
+  resultComputer_p.innerHTML = `Enemy picks ${convertToWord(computerChoice)}`;
+  result_p.innerHTML = `You Lose!`;
+
+  userChoice_div.classList.add("red-glow");
+  setTimeout(() => {
+    userChoice_div.classList.remove("red-glow");
+  }, 400);
+}
+
+function draw(userChoice, computerChoice) {
+  const userChoice_div = document.getElementById(userChoice);
+  resultUser_p.innerHTML = `You pick  ${convertToWord(userChoice)}`;
+  resultComputer_p.innerHTML = `Enemy picks ${convertToWord(computerChoice)}`;
+  result_p.innerHTML = `Draw!!`;
+
+  userChoice_div.classList.add("gray-glow");
+  setTimeout(() => {
+    userChoice_div.classList.remove("gray-glow");
+  }, 400);
+}
---
+++ notes.md
@@ -0,0 +1,9 @@
+base: Coockie clicker based on rock paper scrissors
+
+[] Save the results to local storage
+[] Make a login page someday to save the data
+[] Add jojo atmosphere
+[] Add HP bar. X loses when hp gets <= 0
+[] Add gatchi skills
+[] Rating System
+[] NANI! sound
